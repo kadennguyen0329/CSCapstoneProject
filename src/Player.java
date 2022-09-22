16a01eedd2fb393a13917a1854cc590bda68dea5
TSSpec <Yoda@fcps.edu>
2022-09-22 11:05:15 -0400

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -4,7 +4,7 @@ public class Player extends Character
 
    public Player(int xCoord, int yCoord, int h, int w, String imageFileName, int health1, int speed1, int strength1, String name1)
    {
-      super(xCoord, yCoord, h, w, imageFileName, health1, speed1, strength1, name1);
+      super(xCoord, yCoord, h, w, imageFileName, name1, speed1, strength1, health1);
       
    }
 
