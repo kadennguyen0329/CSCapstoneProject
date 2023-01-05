46c87ebd5f59ca7ee4e61a4cc5d6704e43df029a
nathanr9 <114169637+nathanr9@users.noreply.github.com>
2023-01-05 09:59:38 -0500

    Update Player.java
    Hiding

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,21 @@
 public class Player extends Character
 {
+   private boolean isHiding;
    public Player(int xCoord, int yCoord, int w, int h, String imageFileName, int health1,int strength1 , int speed1, String name1)
    {
       super(xCoord, yCoord, w, h, imageFileName, speed1, strength1, health1, name1);
+      isHiding = false;
       
+   }
+   
+   public boolean getIsHiding()
+   {
+      return isHiding;
+   }
+   
+   public void switchIsHiding()
+   {
+      isHiding = !isHiding;
    }
    
 }
